077d0a69d95cf54a261de1c4f03c0186ee0d7f93
thomasHM21 <thomas.forsberg@hm.com>
2021-03-01 10:37:34 +0100

    Project init

---
+++ src/test/java/com/example/simplewebcalc/SimpleWebCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplewebcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWebCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
