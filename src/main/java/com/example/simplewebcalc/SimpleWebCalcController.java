496689cb0cd005303c2e2d947372cdb6dc509a79
thomasHM21 <thomas.forsberg@hm.com>
2021-03-01 10:45:02 +0100

    Added add method

---
+++ src/main/java/com/example/simplewebcalc/SimpleWebCalcController.java
@@ -0,0 +1,40 @@
+package com.example.simplewebcalc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class SimpleWebCalcController {
+
+    //Level 1
+    @GetMapping("/")
+    public String level1(Model model, @RequestParam int number1, @RequestParam int number2) {
+        int sum = number1 + number2;
+        model.addAttribute("number1", number1);
+        model.addAttribute("number2", number2);
+        model.addAttribute("sum", sum);
+        return "level1";
+    }
+
+    //Level 2
+    @GetMapping("/add")
+    public String level2() {
+        return "add";
+    }
+
+    @PostMapping("/add")
+    public String level2post(HttpSession session, @RequestParam Integer number) {
+        Integer total = (Integer) session.getAttribute("total");
+        if (total == null) {
+            total = number;
+        } else total = total + number;
+        session.setAttribute("total", total);
+
+        return "add";
+    }
+}
